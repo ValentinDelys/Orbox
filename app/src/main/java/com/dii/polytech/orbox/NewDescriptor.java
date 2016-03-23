0e677f7526f2f83e5a2d820dc8691b4f49b10fb3
Valentin <valentin.delys@gmail.com>
2016-03-23 10:01:59 +0100

    Orbox - Amélioration de l'activité NewDescriptor

--- app/src/main/java/com/dii/polytech/orbox/NewDescriptor.java
+++ app/src/main/java/com/dii/polytech/orbox/NewDescriptor.java
@@ -1,8 +1,6 @@
 package com.dii.polytech.orbox;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -39,15 +37,6 @@ public class NewDescriptor extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         buttonCancel = (Button) findViewById(R.id.NewDescriptor_ButtonCancel);
         buttonOK = (Button) findViewById(R.id.NewDescriptor_ButtonOk);
